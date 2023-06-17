7880bf388a261cabf7f30ff9e011dee4da905722
yindaheng98 <yindaheng98@gmail.com>
2023-06-17 13:26:29 +0800

    加文

--- citation_crawler/summarizers/neo4j.py
+++ citation_crawler/summarizers/neo4j.py
@@ -1,7 +1,5 @@
 import logging
 import re
-import os
-from typing import Optional
 from citation_crawler import Summarizer, Paper
 
 from neo4j import Session
@@ -9,6 +7,33 @@ from neo4j import Session
 '''Use with dblp-crawler'''
 
 logger = logging.getLogger("graph")
+
+
+def add_paper(tx, paper: Paper):
+    dblp_id = paper.dblp_id()
+    doi = paper.doi()
+    if doi:
+        if not re.match(r"^https*://doi.org/", doi):
+            doi = "https://doi.org/" + doi
+    if dblp_id and doi:
+        tx.run("MERGE (p:Publication {key: $key}) "
+               "ON CREATE SET p.title=$title, p.year=$year, p.doi=$doi",
+               key=dblp_id,
+               title=paper.title(),
+               year=paper.year(),
+               doi=doi)
+    elif dblp_id:
+        tx.run("MERGE (p:Publication {key: $key}) "
+               "ON CREATE SET p.title=$title, p.year=$year",
+               key=dblp_id,
+               title=paper.title(),
+               year=paper.year())
+    elif doi:
+        tx.run("MERGE (p:Publication {doi: $doi}) "
+               "ON CREATE SET p.title=$title, p.year=$year",
+               doi=doi,
+               title=paper.title(),
+               year=paper.year())
 
 
 def match_statement(p: Paper):
@@ -28,7 +53,7 @@ def add_reference(tx, a: Paper, b: Paper):
     stb, vlb = match_statement(b)
     if sta is None or stb is None:
         return
-    statement = (sta.format(n='a')) + (stb.format(n='b')) + "MERGE (a)-[:CITE]->(b)"
+    statement = sta.format(n='a') + stb.format(n='b') + "MERGE (a)-[:CITE]->(b)"
     tx.run(statement, a=vla, b=vlb)
 
 
@@ -38,7 +63,7 @@ class Neo4jSummarizer(Summarizer):
         self.session = session
 
     async def write_paper(self, paper) -> None:
-        print(paper.__dict__())
+        self.session.execute_write(add_paper, paper)
 
     async def write_reference(self, paper, reference) -> None:
         self.session.execute_write(add_reference, paper, reference)
